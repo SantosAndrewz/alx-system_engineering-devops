410cc821000f82a574b4c4849f3ee53f38d5cba9
SantosAndrewz <sentosandrewz@gmail.com>
2024-04-23 11:51:27 -0700

    added module comment

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Uses a given REST API, returns information about his/her TODO list progress.
+'''Uses a given REST API, returns information about his/her TODO progress.'''
 
 import requests
 import sys
@@ -18,12 +18,12 @@ def gather_employee_todo_progress(userId):
     Prints employee's name, tasks completed and title, total tasks, progress.
     '''
 
-    # REST API urls given
+    '''REST API urls given'''
     original_url = 'https://jsonplaceholder.typicode.com'
     todos_url = f"{original_url}/todos?userId={userId}"
     user_url = f"{original_url}/users/{userId}"
 
-    # Checking if userId provided is an integer
+    '''Checking if userId provided is an integer'''
     try:
         userId = int(userId)
     except ValueError:
@@ -31,27 +31,27 @@ def gather_employee_todo_progress(userId):
         sys.exit(1)
 
     try:
-        # Using the provided REST API to gather employee information:
+        '''Using the provided REST API to gather employee information'''
         user_data = requests.get(user_url)
         user_data_json = user_data.json()
         user_name = user_data_json["name"]
 
-        # Getting todo list of the employees.
+        '''Getting todo list of the employees.'''
         todos_data = requests.get(todos_url)
         todos = todos_data.json()
 
-        # Computing total tasks and tasks completed.
+        '''Computing total tasks and tasks completed.'''
         total_tasks = len(todos)
         tasks_completed = (todo for todo in todos if todo['completed'])
         num_tasks_completed = len(tasks_completed)
 
-        # Computing todos progress of the employee
+        '''Computing todos progress of the employee'''
         todos_progress = (num_tasks_completed/total_tasks)
 
-        # Displaying todos progress
+        '''Displaying todos progress'''
         print(f'Employee {user_name} is done with tasks ({todos_progress}):')
 
-        # Displaying the task title
+        '''Displaying the task title'''
         for task in tasks_completed:
             print(f"\t {task['title]}")
 
