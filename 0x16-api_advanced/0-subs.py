077cbcc68acaf70b3f76d14d7ee5caf60962ea7d
SantosAndrewz <sentosandrewz@gmail.com>
2024-05-07 18:03:01 -0700

    more on num_ subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,12 +19,12 @@ def number_of_subscribers(subreddit):
         int: Number of of subscribers if subreddit valid else 0.
     """
 
-    if not  isinstance(subreddit, str) or not subreddit:
+    if not isinstance(subreddit, str) or not subreddit:
         return 0
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    usr_header = {"User-Agent": "Mozilla/5.0"}
-    rsp = requests.get(url, headers=usr_header, allow_redirects=False).json()
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    usr_header = {"User-Agent": "My_user"}
+    rsp = requests.get(url, headers=usr_header, allow_redirects=False)
 
     if rsp.ok:
         try:
@@ -35,6 +35,6 @@ def number_of_subscribers(subreddit):
             print(f"Response text: {rsp.text}")
             return 0
     else:
-        print(f"Request failed with status code {response.status_code}")
+        print(f"Request failed with status code {rsp.status_code}")
         print(f"Response text: {rsp.text}")
         return 0
