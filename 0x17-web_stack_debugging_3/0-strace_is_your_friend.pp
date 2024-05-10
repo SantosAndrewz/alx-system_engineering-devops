c711449aa2841be11312b423f836ed44cc2c87f8
SantosAndrewz <sentosandrewz@gmail.com>
2024-05-09 19:41:27 -0700

    debug 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 #A script fixing apache 500 error
 exec { 'fix-wordpress':
-	command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/\
-/var/www/html/wp-settings.php; service apache2 restart"',
-	path => '/usr/bin:usr/sbin:bin'
+	command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/\
+/var/www/html/wp-settings.php',
+	path => '/usr/bin:usr/sbin:bin',
+	subscribe => File['/var/www/html/wp-settings.php'],
 }
