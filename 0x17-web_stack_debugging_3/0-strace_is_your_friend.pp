1a702312a2410230fd6a0071344674ad3fac6808
SantosAndrewz <sentosandrewz@gmail.com>
2024-05-12 19:59:19 -0700

    more on the command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 #A script fixing apache 500 error
 exec { 'fix-wordpress':
 	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => '/usr/local/bin/:/bin/',
-	subscribe => File['/var/www/html/wp-settings.php']
+	path => '/usr/local/bin/:/bin/'
 }
