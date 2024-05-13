0a14e2e449aa37c7a73fbcd0fd08bbb42ea29717
SantosAndrewz <sentosandrewz@gmail.com>
2024-05-12 17:47:34 -0700

    modified path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 #A script fixing apache 500 error
 exec { 'fix-wordpress':
-	command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/\
-/var/www/html/wp-settings.php',
-	path => '/usr/bin:usr/sbin:bin',
+	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path => '/usr/local/bin/:/bin/',
 	subscribe => File['/var/www/html/wp-settings.php'],
 }
