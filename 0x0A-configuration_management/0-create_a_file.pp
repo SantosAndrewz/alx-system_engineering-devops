0a491fc3be81c72644ac15654046b7cac7a5aa7e
SantosAndrewz <sentosandrewz@gmail.com>
2024-05-21 05:38:44 -0700

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# a script that creates a file in /tmp,
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
